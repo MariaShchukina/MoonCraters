1b176c34c0f612819bc2a629a9d3039fe0dcb629
v_pike <vsevolod.shchukin@gmail.com>
2022-05-15 12:34:19 +0300

    Created readFile func

--- src/main/kotlin/MoonCraters.kt
+++ src/main/kotlin/MoonCraters.kt
@@ -1,3 +1,23 @@
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
 fun main() {
-    
+    readFile()
+}
+
+
+fun readFile() {
+    val file = File("craters.txt")
+    val bufferText = BufferedReader(FileReader(file))
+    val arrayOfArray = mutableListOf<MutableList<Int>>()
+    var line: String
+    while (true) {
+        line = bufferText.readLine() ?: break
+        val array = mutableListOf<Int>()
+        for (element in line) {
+            array.add(element.digitToInt())
+        }
+        arrayOfArray.add(array)
+    }
 }
